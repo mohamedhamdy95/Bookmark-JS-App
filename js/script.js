79b3e1a0f9dbb19c2f49f5786f7aa98d4d2349c6
mohamedhamdy95 <mohamed.hamdy9995@gmail.com>
2023-07-23 13:47:46 +0300

    init done

---
+++ js/script.js
@@ -0,0 +1,76 @@
+var siteName = document.getElementById("sName");
+var siteUrl = document.getElementById("sUrl");
+var addBtn = document.getElementById("add");
+var sitsList = [];
+
+if (localStorage.getItem("siteData" == null)) {
+  sitsList = [];
+} else {
+  sitsList = JSON.parse(localStorage.getItem("siteData"));
+  display();
+}
+// add data funnction
+addBtn.onclick = function () {
+  sitData = {
+    siteName: siteName.value,
+    sitUrl: siteUrl.value,
+  };
+  if (urlValidation() === true) {
+    sitsList.push(sitData);
+    localStorage.setItem("siteData", JSON.stringify(sitsList));
+    display();
+    reset();
+  } else {
+    alert("Enter avalid input like : https://www.google.com");
+  }
+  console.log(sitsList);
+};
+// URL Validation
+function urlValidation() {
+  var urlRegex = /^(https:\/\/)(www\.)[a-z|A-Z]+(\.com)$/;
+  var urlTest = siteUrl.value;
+  if (urlRegex.test(urlTest)) {
+    console.log("maaatch");
+    return true;
+  } else {
+    console.log("noo");
+    return false;
+  }
+}
+// display data funnction
+function display() {
+  var trs = ``;
+  for (var i = 0; i < sitsList.length; i++) {
+    trs += `
+      <tr >
+        <td>${i + 1}</td>
+        <td>${sitsList[i].siteName}</td>
+        <td>
+          <button class="btn btn-outline-info">
+          <a class="text-decoration-none " target="_blank" 
+          href="${
+            sitsList[0].sitUrl
+          }"><i class="fa-solid fa-eye"></i> Visit </a>
+          </button>
+        </td>
+        <td>
+          <button class="btn btn-outline-danger" onclick="deleteData(${i})">
+            <i class="fa-solid fa-trash"></i> Delete
+          </button>
+        </td>
+      </tr>   
+    `;
+  }
+  document.getElementById("tBody").innerHTML = trs;
+}
+// delete funnction
+function deleteData(index) {
+  sitsList.splice(index, 1);
+  localStorage.setItem("siteData", JSON.stringify(sitsList));
+  display();
+}
+// reset funnction
+function reset() {
+  siteName.value = "";
+  siteUrl.value = "";
+}
